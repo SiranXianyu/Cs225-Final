c5604a8a93a6183f11450ca6a7f9cfdffb0030c5
SiranXianyu <92840660+SiranXianyu@users.noreply.github.com>
2022-11-29 10:21:01 -0600

    Update graph.cpp

--- src/graph.cpp
+++ src/graph.cpp
@@ -132,7 +132,7 @@ void Graph::ConnectedComponents() {
     for (unsigned school_Number = 0; school_Number < nodes_.size(); school_Number++) {
         //if the school has not been visited
         if (visited[school_Number] == false) {
-            std::unordered_set<Node> one_connected_component = BFS(school_Number, visited);
+            std::vector<Node> one_connected_component = BFS(school_Number, visited);
             connected_components.push_back(one_connected_component);
         }
     }
@@ -140,15 +140,15 @@ void Graph::ConnectedComponents() {
 
 }
 
-std::unordered_set<Node> Graph::BFS(unsigned school_Number, bool visited[]) {
+std::vector<Node> Graph::BFS(unsigned school_Number, bool visited[]) {
     //mark the current school node as visited
     visited[school_Number] = true;
     std::queue<Node> schools;
     schools.push(nodes_.at(school_Number));
-    std::unordered_set<Node> output_set;
+    std::vector<Node> output_set;
     while (!schools.empty()) {
         Node n = schools.front();
-        output_set.insert(n);
+        output_set.push_back(n);
         schools.pop();
         std::vector<Node> neighbors = adjacency_lists_.at(n.school_number);
         for (auto i: neighbors) {
@@ -162,4 +162,3 @@ std::unordered_set<Node> Graph::BFS(unsigned school_Number, bool visited[]) {
     
 
 }
-
