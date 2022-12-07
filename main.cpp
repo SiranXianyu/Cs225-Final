77bc3303d91e82bcb7936165e9ee2eda14d59fdc
SiranXianyu <92840660+SiranXianyu@users.noreply.github.com>
2022-12-06 18:18:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,14 @@
 int main () {
     
     Graph graph;// error in this line
-    
-    for (unsigned k = 0; k < 9; k++) {
-        std::cout << "hello" << std::endl;
+    graph.Build();
+    graph.ConnectedComponents();
+    std::vector<std::vector<Node> > connected_component = graph.getConnectedComponents();
+    for (unsigned i = 0; i < connected_component.size(); i++) {
+        for (unsigned j = 0; j < connected_component.at(i).size(); j++) {
+            std::cout << connected_component.at(i).at(j).name << ", ";
+        }
+        std::cout << std::endl;
     }
-    
-    //Cout all the schools
-    //for (unsigned k = 0; k < schools_list.size(); k++) {
-    //    std::cout << "hello" << std::endl;
-    //    std::cout << schools_list.at(k).name << std::endl;
-    //}
-    //for (unsigned k = 0; k < 9; k++) {
-    //    std::cout << "hello" << std::endl;
-    //}
-    //return 0;
+    return 0;
 }
