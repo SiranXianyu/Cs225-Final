874827014118aee6875c29750ba76c680ee9c88c
SiranXianyu <92840660+SiranXianyu@users.noreply.github.com>
2022-12-08 15:17:22 -0600

    Update graph.h

--- src/graph.h
+++ src/graph.h
@@ -2,12 +2,14 @@
 //we store data in adjacency lists
 #pragma once
 
+#include "draw.hpp"
 #include <string>
 #include <vector>
 #include <map>
 #include <unordered_set>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 #include <set>
 #include <iostream>
 #include <cmath>
@@ -19,8 +21,23 @@ struct Node {
     //the school number is based on the order we read data
     unsigned school_number;
     std::string name;
+    unsigned x;
+    unsigned y;
     unsigned cited_times;
+    double centrality;
 };
+
+struct Red {
+    unsigned r = 252;
+    unsigned g = 25;
+    unsigned b = 63;
+}
+
+struct Grey {
+    unsigned r = 100;
+    unsigned g = 100;
+    unsigned b = 100;
+}
 
 class Graph {
     public:
@@ -37,6 +54,11 @@ class Graph {
         ReadSchools();
         ReadEdges();
         RefineLists();
+        for (unsigned x = 0; x < 1000; x++) {
+            for (unsigned y = 0; y < 1000; y++) {
+                image[x][y] = 0;
+            }
+        }
     }
     
     void ReadRelations() {
@@ -83,6 +105,8 @@ class Graph {
         for (unsigned i = 0; i < schools.size() - 1; i++) {
                 Node college;
                 college.school_number = i;
+                college.centrality = 0;
+                college.x = 0; college.y = 0;
                 college.name = schools.at(i).first;
                 college.cited_times = schools.at(i).second;
                 nodes_.push_back(college);
@@ -206,6 +230,91 @@ class Graph {
         }
         return output;
     }
+
+
+    //store netforce of one connected component into a 2d vector, 
+    std::vector<<std::pair<double, double>> CalculateNetForce(std::vector<Node> cc) {
+        cc = Distribute(cc);
+        std::vector<<std::pair<double, double>> nf_cc;
+        for (unsigned k = 0; k < cc.size(); k++) {
+            Node school = cc.at(k);
+            std::pair<double, double> netforce(0, 0);
+            //calculate the netforce of a single vertex in the whole connected components
+            std::vector<Node> neighbors = adjacency_lists_.at(school.school_number);
+            for (unsigned l = 0; l < neighbors.size(); l++) {
+                std::pair<double, double> attractive_force = CalculateAttractive(school, neighbors.at(i));
+                std::pair<double, double> repulsive_force = CalculateReplusive(school, neighbors.at(i));
+                //add direction
+                double direction_attractive_x = (neighbors.at(i).x - school.x) / std::abs(neighbors.at(i).x - school.x);
+                double direction_attractive_y = (neighbors.at(i).y - school.y) / std::abs(neighbors.at(i).y - school.y);
+                attractive_force.first = attractive_force.first * direction_attractive_x;
+                attractive_force.second = attractive_force.second * direction_attractive_y;
+
+                double direction_repulsive_x = (school.x - neighbors.at(i).x) / std::abs(neighbors.at(i).x - school.x);
+                double direction_repulsive_y = (school.y - neighbors.at(i).y) / std::abs(neighbors.at(i).y - school.y);
+                repulsive_force.first = repulsive_force.first * direction_repulsive_x;
+                repulsive_force.second = repulsive_force.second * direction_repulsive_y;
+
+                netforce.first += attractive_force.first;
+                netforce.first += repulsive_force.first;
+                netforce.second += attractive_force.second;
+                netforce.second += repulsive_force.second;
+                
+            }
+            nf_cc.push_back(netforce);
+        }
+    }
+
+
+    //accepts the original connected components, returns the connected components with new location
+    std::vector<Node> Move(std::vector<Node> cc) {
+        double max_d = 5;
+        double min_d = -5;
+        std::vector<Node> updated_cc;
+        std::vector<<std::pair<double, double>> nf_cc = CalculateNetForce(cc);
+        for (unsigned k = 0; k < cc.size(); k++) {
+            Node school = cc.at(i);
+            std::pair<double, double> netforce = nf_cc.at(k);
+            double time = 1
+            double d_x = netforce.first * time * time  * / (2 * school.centrality);
+            double d_y = netforce.second * time * time  * / (2 * school.centrality);
+            if (d_x > max_d) d_x = max_d;
+            if (d_y > max_d) d_y = max_d;
+            if (d_x < min_d) d_x = min_d;
+            if (d_y < min_d) d_y = min_d;
+            school.x += std::round(d_x);
+            school.y += std::round(d_y);
+            updated_cc.push_back(school);
+        }
+    }
+
+    void Draw(std::vector<Node> cc) {
+        //Selection sort: sort the connected component from low centrality to high centrality
+        for (unsigned k = 0; k < cc.size() - 1; k++) {
+            Node n1 = cc.at(k);
+            for (unsigned l = k + 1; l < cc.size(); l++) {
+                if (cc.at(l).centrality < n1.centrality) {
+                    Node temp = cc.at(l);
+                    cc.at(l) = n1;
+                    n1 = temp;
+                }
+            }
+        }
+
+        unsigned base_size = 5; 
+        for (unsigned k = 0; k < cc.size(); k++) {
+            Node school = cc.at(k);
+            image[school.x][school.y] = true;
+            for (unsigned i = school.x - k - 5; i <= school.x + k + 5) {
+                for (unsigned j = school.y - k - 5; j <= school.y + k + 5) {
+                    if (std::sqrt((school.x - i) * (school.x - i) + (school.y - j) * (school.y - j)) <= 5) {
+                        image[i][j] = true;
+                    }
+                }
+            }
+        }
+    }
+
     
     //getter functions:
     std::vector<Node> getNodes() {
@@ -221,18 +330,25 @@ class Graph {
     }
     
 
+    unsigned* getImage() {
+        return image;
+    }
+
 
     private:
-
-    //refined data used for future algorithms
-    std::vector<Node> nodes_;
-    std::vector<std::vector<Node> > adjacency_lists_;
-
 
     //raw data of universities stored in pairs
     std::vector<std::pair<std::string, int> > schools;
     std::vector<std::pair<std::string, std::string>> relations;
 
+
+    //refined data used for future algorithms
+    std::vector<Node> nodes_;
+    std::vector<std::vector<Node> > adjacency_lists_;
+
     //data after BFS for searchwing for connected components
     std::vector<std::vector<Node> > connected_components;
+
+    //if true, color with red, else color with green
+    bool image[1000][1000];
 };
